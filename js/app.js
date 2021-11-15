d88448e768073f530a63af082cf8dd60bf71d335
Diefonro <dfonsecar@unal.edu.co>
2021-11-15 02:37:55 -0500

    test6

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 function clickDD() {
     const dropdown = document.querySelector('.dd');
   dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+
+  const dropdown = document.querySelector('.dd-cont');
+  dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
 }
